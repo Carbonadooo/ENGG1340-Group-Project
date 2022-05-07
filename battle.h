372ff26ff8c2aa7e69cfec42f4cf5c54cf68c326
Carbonadooo <87820073+Carbonadooo@users.noreply.github.com>
2022-05-07 20:39:57 +0800

    Update battle.h

--- battle.h
+++ battle.h
@@ -7,5 +7,5 @@
 // battle.h
 #ifndef BATTLE_H 
 #define BATTLE_H 
-bool battle(Hero& Ian, bool boss, string Curskills[4],skills,enemy);
+bool battle(Hero& Ian, bool boss, string Curskills[4],map<string,Ability> skills,map<string,Monster> enemy);
 #endif
