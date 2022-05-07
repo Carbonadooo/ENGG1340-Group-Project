eb39abf7ac25467fd6725f09a8f924429515af87
Carbonadooo <87820073+Carbonadooo@users.noreply.github.com>
2022-05-07 21:09:50 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -66,6 +66,51 @@ void imp() {//import data
 	fin.close();
 }
 
+void ReadInArchive(Hero& Ian, int x, int& level, string CurSkills[4]) {
+	Ability tempSkill;
+	string command;
+	ifstream fin;
+	if (x==1) {
+    	fin.open("DefaultArchive.txt");
+    	cout<<"\t\tStarting a new game...\n";
+    	sleep(2);
+  	}
+ 	else {
+    	fin.open("PlayerArchive.txt");
+   		fin>>command;
+    	if(command=="dead"){
+      	cout<<"\t\tYou don't have an archive. Starting a new game...\n";
+      	sleep(2);
+      	fin.close();
+      	fin.open("DefaultArchive.txt");
+		}
+   		else{
+     		cout<<"\t\tLoading the last game.\n";
+    		sleep(2);
+    	}
+	}
+	while (fin >> command) {
+		if (command == "Level")  fin >> level;
+		if (command == "MaxHP") fin >> Ian.MaxHP;
+		if (command == "CurHP") fin >> Ian.CurHP;
+		if (command == "Attack") fin >> Ian.ATK;
+		if (command == "Ability") {
+			fin >> command;
+			while (command != "CurrentSkill") {
+				Ian.abilities.push_back(command);
+				skills[command].learned = 1;
+				fin >> command;
+			}
+		}
+		if (command == "CurrentSkill"){
+      		command="";
+			for (int i = 0; i < 4; i++) fin >> CurSkills[i];
+      	}
+	}
+  fin.close();
+}
+
+
 char GetOption() {
 	cout << "Enter number to make choice: " << endl;
 	char key = _getch();
