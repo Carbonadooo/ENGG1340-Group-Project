d89e450874d686c6d840680324c85f31c3b33e9c
Carbonadooo <87820073+Carbonadooo@users.noreply.github.com>
2022-05-07 20:29:17 +0800

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -2,39 +2,31 @@
 #include<map>
 #include"data.h"
 
-void imp(map<string,Ability>& skills, map<string,Monster>& enemy) {//import data
-	Ability tempSkill;
-	Monster tempMonster;
-	string tName;
-	ifstream fin;
-	fin.open("ability.txt", ios::in); //import abilities from the files
-	if (fin.fail()) {
-		cout << "Error in file opening!" << endl;
-		exit(1);
-	}
-	while (fin >> tName) {
-		fin >> tempSkill.damage >> tempSkill.heal >> tempSkill.mana;
-		fin >> tempSkill.Peff >> tempSkill.Eeff;
-		tempSkill.learned = 0;
-		skills[tName] = tempSkill;
-	}
-	fin.close();
 
-	fin.open("monster.txt", ios::in); //import monsters from the files
-	if (fin.fail()) {
-		cout << "Error in file opening!" << endl;
-		exit(1);
-	}
-	while (fin >> tName) {
-		fin >> tempMonster.HP >> tempMonster.ATK >> tempMonster.Peff >> tempMonster.Eeff;
-		enemy[tName] = tempMonster;
-	}
-	fin.close();
-}
 
-void ReadInArchive(Hero& Ian, ifstream& fin, int& level, string CurSkills[4], map<string, Ability>& skills) {
+void ReadInArchive(Hero& Ian, int x, int& level, string CurSkills[4]) {
 	Ability tempSkill;
 	string command;
+	ifstream fin;
+	if (x==1) {
+    	fin.open("DefaultArchive.txt");
+    	cout<<"\t\tStarting a new game...\n";
+    	sleep(2);
+  	}
+ 	else {
+    	fin.open("PlayerArchive.txt");
+   		fin>>command;
+    	if(command=="dead"){
+      	cout<<"\t\tYou don't have an archive. Starting a new game...\n";
+      	sleep(2);
+      	fin.close();
+      	fin.open("DefaultArchive.txt");
+		}
+   		else{
+     		cout<<"\t\tLoading the last game.\n";
+    		sleep(2);
+    	}
+	}
 	while (fin >> command) {
 		if (command == "Level")  fin >> level;
 		if (command == "MaxHP") fin >> Ian.MaxHP;
@@ -48,15 +40,18 @@ void ReadInArchive(Hero& Ian, ifstream& fin, int& level, string CurSkills[4], ma
 				fin >> command;
 			}
 		}
-		if (command == "CurrentSkill")
-			for (int i = 0; i < 4; i++)
-				fin >> CurSkills[i];
+		if (command == "CurrentSkill"){
+      		command="";
+			for (int i = 0; i < 4; i++) fin >> CurSkills[i];
+      	}
 	}
+  fin.close();
 }
 
 void Save(Hero Ian, int level, string CurSkills[4]) {
 	ofstream fout;
 	fout.open("PlayerArchive.txt");
+	fout << "alive"  << endl;
 	fout << "Level " << level << endl;
 	fout << "MaxHP " << Ian.MaxHP << endl;
 	fout << "CurHP " << Ian.CurHP << endl;
@@ -68,7 +63,7 @@ void Save(Hero Ian, int level, string CurSkills[4]) {
 		fout << *itr << " ";
 	}
 	cout << endl;
-	fout << "CurrentSkills" << endl;
+	fout << "CurrentSkill" << endl;
 	for (int i = 0; i < 4; i++) {
 		fout << CurSkills[i] << " ";
 	}
