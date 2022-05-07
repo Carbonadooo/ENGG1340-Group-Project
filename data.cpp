592e239584bd1a17c63774f19223e28684cb085f
Carbonadooo <87820073+Carbonadooo@users.noreply.github.com>
2022-05-07 21:09:14 +0800

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -4,51 +4,6 @@
 #include"data.h"
 
 
-
-void ReadInArchive(Hero& Ian, int x, int& level, string CurSkills[4]) {
-	Ability tempSkill;
-	string command;
-	ifstream fin;
-	if (x==1) {
-    	fin.open("DefaultArchive.txt");
-    	cout<<"\t\tStarting a new game...\n";
-    	sleep(2);
-  	}
- 	else {
-    	fin.open("PlayerArchive.txt");
-   		fin>>command;
-    	if(command=="dead"){
-      	cout<<"\t\tYou don't have an archive. Starting a new game...\n";
-      	sleep(2);
-      	fin.close();
-      	fin.open("DefaultArchive.txt");
-		}
-   		else{
-     		cout<<"\t\tLoading the last game.\n";
-    		sleep(2);
-    	}
-	}
-	while (fin >> command) {
-		if (command == "Level")  fin >> level;
-		if (command == "MaxHP") fin >> Ian.MaxHP;
-		if (command == "CurHP") fin >> Ian.CurHP;
-		if (command == "Attack") fin >> Ian.ATK;
-		if (command == "Ability") {
-			fin >> command;
-			while (command != "CurrentSkill") {
-				Ian.abilities.push_back(command);
-				skills[command].learned = 1;
-				fin >> command;
-			}
-		}
-		if (command == "CurrentSkill"){
-      		command="";
-			for (int i = 0; i < 4; i++) fin >> CurSkills[i];
-      	}
-	}
-  fin.close();
-}
-
 void Save(Hero Ian, int level, string CurSkills[4]) {
 	ofstream fout;
 	fout.open("PlayerArchive.txt");
