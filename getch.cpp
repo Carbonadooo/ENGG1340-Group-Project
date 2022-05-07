31aa8dc14243e50fe192a4a3c60ab4e8959776cb
Carbonadooo <87820073+Carbonadooo@users.noreply.github.com>
2022-05-07 21:17:50 +0800

    Create getch.cpp

---
+++ getch.cpp
@@ -0,0 +1,20 @@
+#include"getch.h"
+#include<iostream>
+using namespace std;
+
+int _getch(void)
+{
+    struct termios tm, tm_old;
+    int fd = 0, chr;
+
+    if (tcgetattr(fd, &tm) < 0) return -1;
+
+    tm_old = tm;
+    cfmakeraw(&tm);
+    if (tcsetattr(fd, TCSANOW, &tm) < 0) return -1;
+
+    chr = getchar();
+    if (tcsetattr(fd, TCSANOW, &tm_old) < 0) return -1;
+
+    return chr;
+}
